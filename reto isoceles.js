94278a34940cd283a973129d1d8907349d3a99bf
LuchoCb <lucho.kbzas@icloud.com>
2022-04-25 09:05:55 -0500

    AJUSTE FUNCIONAL HTML

--- reto isoceles.js
+++ reto isoceles.js
@@ -4,5 +4,16 @@ function alturaTI (lado1, lado2, Base){
     else alert ("No es isoceles");
 }
 
+function calcularAlturaTI(){
+    const input = document.getElementById ("InputL1T"); //nos permite obtener el elemento HTML//
+    const Input2 = document.getElementById ("InputL2T");
+    const Input3 = document.getElementById ("InputBT");
+    const value1 = Number(input.value);
+    const value2 = Number (Input2.value);
+    const value3 = Number (Input3.value);
+
+    const AlturaTI = alturaTI (value1,value2,value3);
+}
+
 // 5.9160797830996169// El del man
 
